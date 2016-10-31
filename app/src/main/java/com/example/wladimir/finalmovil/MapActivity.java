732b99c212dcd02ba049262249c12ade0f37a688
Wladimir <wrodriguez@unac.edu.co>
2016-10-31 09:31:25 -0500

    Initial commit

---
+++ app/src/main/java/com/example/wladimir/finalmovil/MapActivity.java
@@ -0,0 +1,18 @@
+package com.example.wladimir.finalmovil;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+
+/**
+ * Created by Wladimir on 30/10/2016.
+ */
+public class MapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+
+}
